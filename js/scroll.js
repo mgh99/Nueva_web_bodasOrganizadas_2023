a6669b3b25ba021eb6facf6f83cc3dead64908ea
María González Herrero <43043718+mgh99@users.noreply.github.com>
2023-03-19 12:58:38 +0100

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -1,4 +1,16 @@
 function scrollToSection(sectionId) {
+    console.log(sectionId);
+
     var section = document.getElementById(sectionId);
-    section.scrollIntoView({ behavior: "smooth" });
+    console.log(section);
+    var sectionOffse = section.offsetHeight;
+
+    if (sectionOffse > 0) {
+        console.log(sectionOffse);
+        window.scroll({top: section.sectionOffse , left: 0, behavior: 'smooth'});
+        //window.scrollTo({ top: 1700, behavior: "smooth" });
+      
+
+    }
+
 }
